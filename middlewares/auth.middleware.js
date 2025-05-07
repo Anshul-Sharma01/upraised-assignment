12d13e356f35f537699b224f8936bfa44ca15b0e
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-05-07 23:52:11 +0530

    feat: implement JWT verification middleware and define User and Gadget models with password hashing

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError.js";
+
+
+
+export const verifyJwtMiddleware = asyncHandler(async(req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if(!token){
+            throw new ApiError(401, "No token provided"); 
+        }
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decodedToken;
+        next();
+    } catch (error) {
+        throw new ApiError(400, "Error occurred at backend");
+    }
+})
